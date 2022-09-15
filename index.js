09e553f2344ecae32f7d476ffdcb349ac29ef4d8
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-09-15 08:57:55 -0500

    refactored my js code a little bit

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 const minus = document.getElementById("minus");
 const add = document.getElementById("plus");
 const number1 = document.getElementById("num");
-const image = document.getElementById("image");
-const image1 = document.getElementById("image1");
-const image2 = document.getElementById("image2");
-const image3 = document.getElementById("image3");
-const allImages = [image, image1, image2, image3];
-
+// Get the modal
+const modal = document.getElementById("myModal");
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+const btn1 = document.getElementById("myBtn1");
+const btn2 = document.getElementById("myBtn2");
+const btn3 = document.getElementById("myBtn3");
 let i = 0;
 let amount = i;
 
-// adding button to how many orders you want of the product
-
+// add to how many shoes/product you want
 add.addEventListener("click", function () {
   amount += 1;
   number1.innerHTML = amount;
@@ -23,7 +23,7 @@ minus.addEventListener("click", function () {
   return (number1.innerHTML = amount);
 });
 
-// next / previous controls
+// next / previous controls on modal
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
@@ -52,54 +52,35 @@ function changeSlides(n) {
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
+
 function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName("Slide");
   const slides1 = document.getElementsByClassName("Slide1");
-
-  if (n > slides.length && slides1.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-    slideIndex = slides1.length;
-  }
-
-  // idkk wtf i did tbh lmaoooo i need to make this look better fuck
-  Array.from(slides).forEach((item) => (item.style.display = "none"));
-  slides[slideIndex - 1].style.display = "block";
-  Array.from(slides1).forEach((item) => (item.style.display = "none"));
-  slides1[slideIndex - 1].style.display = "block";
+  const slidesarr = [slides, slides1];
+  slidesarr.forEach(function (arr) {
+    if (n > arr.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = arr.length;
+    }
+    Array.from(arr).forEach((item) => (item.style.display = "none"));
+    arr[slideIndex - 1].style.display = "block";
+  });
 }
-
-// Get the modal
-const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
-const btn1 = document.getElementById("myBtn1");
-const btn2 = document.getElementById("myBtn2");
-const btn3 = document.getElementById("myBtn3");
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.addEventListener("click", function () {
-  modal.style.display = "block";
+const btns = [btn, btn1, btn2, btn3];
+btns.forEach((number) => {
+  number.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
 });
 
-btn1.addEventListener("click", function () {
-  modal.style.display = "block";
-});
-
-btn2.addEventListener("click", function () {
-  modal.style.display = "block";
-});
-
-btn3.addEventListener("click", function () {
-  modal.style.display = "block";
-});
 // When the user clicks on <span> (x), close the modal
 span.addEventListener("click", function () {
   modal.style.display = "none";
