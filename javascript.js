3beec542bdc0af1c18f23d6cfc52ab59824449d7
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-09-25 15:52:26 -0500

    ya

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-
+const itemName = document.querySelector(".productName").innerHTML;
+const basePrice = document.querySelector(".priceValue").innerHTML;
+const itemPic = document.querySelector(".big-img").src;
+console.log(+basePrice);
+console.log(itemName);
+console.log(itemPic);
